25f99d013a06c4f8265faf6888736b1a75ca37bf
walissonssousa <95376015+walissonssousa@users.noreply.github.com>
2022-01-11 16:03:13 -0300

    atualização funcionando

--- whatsappbot.py
+++ whatsappbot.py
@@ -1,43 +1,33 @@
-from selenium import webdriver
 import time
-from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+import pandas as pd
+from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import time
+import urllib
 
 
-driver = webdriver.Chrome(ChromeDriverManager().install())
-driver.get('https://web.whatsapp.com/')
-time.sleep(3)
+contatos_df = pd.read_excel("Enviar.xlsx")
 
 
 
-contatos = ['Mae']
-mensagem = ['Teste Robô']
+navegador = webdriver.Chrome()
+navegador.get("https://web.whatsapp.com/")
+time.sleep(30)
 
-def buscar_contato(contato):
-    campo_pesquisa = driver.find_element_by_xpath(
-        '//div[contains(@class, " copyable-text selectable-text")]')
-    time.sleep(3)
-    campo_pesquisa.click()
-    campo_pesquisa.send_keys(contato)
-    campo_pesquisa.send_keys(Keys.ENTER)
-    
-    
-def enviar_mensagem(mensagem):
-    
-    campo_mensagem= driver[1].find_element_by_xpath(
-        '//div[contains(@class, "copyable-text selectable-text")]')
-    campo_mensagem[1].click()
-    time.sleep(3)
-    campo_mensagem[1].send_keys(mensagem)
-    time.sleep(3)
-    campo_mensagem[1].send_keys(Keys.ENTER)
-    
-    
-    
 
-for contato in contatos:
-    buscar_contato(contato)
-    enviar_mensagem(mensagem)
+for i, mensagem in enumerate(contatos_df['Mensagem']):
+    pessoa = contatos_df.loc[i,"Pessoa"]
+    numero = contatos_df.loc[i,"Número"]
+    texto = urllib.parse.quote( f"oi {pessoa}! {mensagem}")
+    link = f"https://web.whatsapp.com/send?phone={numero}&text={texto}"
+    navegador.get(link)
+    time.sleep(5)
+    navegador.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[2]').send_keys(Keys.ENTER)
+    time.sleep(10)
+
+
+
 
 
 
